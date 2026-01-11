79be633d1ef96abf42fa40a04884238384db8139
zohrehKazemianpour <129424353+zohrehKazemianpour@users.noreply.github.com>
2026-01-11 14:06:07 +0000

    chore: rename main entry point to server.js

---
+++ backend/server.js
@@ -0,0 +1,48 @@
+import express from "express";
+import cors from "cors";
+import {quotes} from "./quotes.js"
+
+
+const app = express();
+app.use(express.json());
+
+const port = 3000;
+
+
+
+function randomQuote() {
+  const index = Math.floor(Math.random() * quotes.length);
+  return quotes[index];
+}
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  const quote = randomQuote();
+  res.json(quote);
+});
+
+app.post("/", (req, res) => {
+  const body = req.body;
+  
+    if (typeof body != "object" || !("quote" in body) || !("author" in body)) {
+      console.error(
+        `Failed to extract quote and author from post body: ${body}`
+      );
+      res
+        .status(400)
+        .send(
+          "Expected body to be a JSON object containing keys quote and author."
+        );
+      return;
+    }
+    quotes.push({
+      quote: body.quote,
+      author: body.author,
+    });
+    res.send("ok");
+  });
+
+app.listen(port, () => {
+  console.error(`Quote server listening on port ${port}`);
+});
